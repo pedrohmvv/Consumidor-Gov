e4bab2af52aa83b471a7210e20eb68f4852b53d6
Pedro Henrique Medeiros Vieira <139015105+pedrohmvv@users.noreply.github.com>
2025-05-08 18:20:24 -0300

    best modeling done

--- src/config.py
+++ src/config.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from yaml import load
-from yaml.loader import SafeLoader
-from os.path import join, dirname, abspath
+from os.path import abspath, dirname, join
+from yaml import load, SafeLoader
 
 @dataclass
 class EnvVariables:
@@ -17,6 +16,20 @@ class ExtractVariables:
     REFERRER: str
     ORIGIN: str
     CARD_CLASS: str
+    ACCEPT: str
+    ACCEPT_ENCODING: str
+    ACCEPT_LANGUAGE: str
+    CONNECTION: str
+    CONTENT_LENGTH: str
+    COOKIE: str
+    HOST: str
+    SEC_CH_UA: str
+    SEC_CH_UA_MOBILE: str
+    SEC_CH_UA_PLATFORM: str
+    SEC_FETCH_DEST: str
+    SEC_FETCH_MODE: str
+    SEC_FETCH_SITE: str
+    X_REQUESTED_WITH: str
 
 class Config:
 
@@ -27,11 +40,25 @@ class Config:
             data = load(file, Loader=SafeLoader)
         self.extract_vars = ExtractVariables(
             API_URL=data.get('API_URL'),
-            CONTENT_TYPE= data.get('CONTENT_TYPE'),
-            USER_AGENT= data.get('USER_AGENT'),
+            CONTENT_TYPE=data.get('CONTENT_TYPE'),
+            USER_AGENT=data.get('USER_AGENT'),
             REFERRER=data.get('REFERRER'),
             ORIGIN=data.get('ORIGIN'),
             CARD_CLASS=data.get('CARD_CLASS'),
+            ACCEPT=data.get('ACCEPT'),
+            ACCEPT_ENCODING=data.get('ACCEPT_ENCODING'),
+            ACCEPT_LANGUAGE=data.get('ACCEPT_LANGUAGE'),
+            CONNECTION=data.get('CONNECTION'),
+            CONTENT_LENGTH=data.get('CONTENT_LENGTH'),
+            COOKIE=data.get('COOKIE'),
+            HOST=data.get('HOST'),
+            SEC_CH_UA=data.get('SEC_CH_UA'),
+            SEC_CH_UA_MOBILE=data.get('SEC_CH_UA_MOBILE'),
+            SEC_CH_UA_PLATFORM=data.get('SEC_CH_UA_PLATFORM'),
+            SEC_FETCH_DEST=data.get('SEC_FETCH_DEST'),
+            SEC_FETCH_MODE=data.get('SEC_FETCH_MODE'),
+            SEC_FETCH_SITE=data.get('SEC_FETCH_SITE'),
+            X_REQUESTED_WITH=data.get('X_REQUESTED_WITH')
         )
         self.env_vars = EnvVariables(
             data_dir=data.get('data_dir'),
